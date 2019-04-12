370eb0557032d863de002f748ed58338445c3b9d
ondrejacke <49538688+ondrejacke@users.noreply.github.com>
2019-04-11 20:43:30 -0400

    Initial Projectiles Commit

---
+++ MovingObject/src/application/Projectiles.java
@@ -0,0 +1,60 @@
+package application;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Projectiles 
+{
+	protected String fileName;
+	protected double xvalue;
+	protected double yvalue;
+	protected int width;
+	protected int height;
+	protected double speed;
+	protected int direction;
+	protected Image image;
+	protected ImageView view;
+}
+
+class Book extends Projectiles
+{
+	public Book()
+	{
+		fileName = "File:///" + System.getProperty("user.dir") + "/stackofbooks.png";
+		width = 100;
+		height = 100;
+		setupImage();
+	}
+	
+	public Book(int x, int y)
+	{
+		fileName = "File:///" + System.getProperty("user.dir") + "/stackofbooks.png";
+		width = 100;
+		height = 100;
+		xvalue = x;
+		yvalue = y;
+		setupImage();
+	}
+	public void setSpeed(int x)
+	{
+		speed = x;
+	}
+	public void setDirection(int x)
+	{
+		direction = x;
+	}
+	public ImageView getImage()
+	{
+		return view;
+	}
+	private void setupImage()
+	{
+		image = new Image(fileName);
+		view = new ImageView(image);
+		view.setX(xvalue); 
+	    view.setY(yvalue);
+		view.setFitHeight(100); 
+	    view.setFitWidth(500); 
+	    view.setPreserveRatio(true); 
+	}
+}
