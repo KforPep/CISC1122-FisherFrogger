ffad60a5aff2733eedca023956f91f9a2d5a4316
Phil Nicholson <nicholsonpk@gmail.com>
2019-05-06 12:54:16 -0400

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -26,13 +26,17 @@ public class Main extends Application{
 	public void start(Stage mainStage) {
 		try {
 			
+			// Set the window to the screen dimensions
 			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 			mainStage.setWidth(screenSize.getWidth()); 		//Set the width of the window to fill screen
 			mainStage.setHeight(screenSize.getHeight());	//Set the height of the window to fill screen
 			
+			// Add the gameplay Grid to the stage
 			Grid gameGrid = new Grid();
 			Pane returnedGamePane = gameGrid.start(mainStage.getHeight());	// Starts new Grid using mainStage's height for sizing
 			
+			// These Panes cover the left and right side of the screen
+			// to hide vehicles entering and exiting the scene
 			Pane leftSideCover = new Pane();
 			leftSideCover.setMaxSize(((mainStage.getWidth() - mainStage.getHeight())/2), mainStage.getHeight());
 			leftSideCover.setMinWidth((mainStage.getWidth() - mainStage.getHeight())/2);
@@ -42,12 +46,13 @@ public class Main extends Application{
 			rightSideCover.setMinWidth((mainStage.getWidth() - mainStage.getHeight())/2);
 			rightSideCover.setBackground(new Background(new BackgroundFill(Color.GREEN, null, null)));
 			
+			// Set left, gameplay, and right on a BorderPane
 			BorderPane totalScreen = new BorderPane();
-			
 			totalScreen.setCenter(returnedGamePane);
 			totalScreen.setRight(rightSideCover);
 			totalScreen.setLeft(leftSideCover);
 			
+			// Set up scene and send all key presses to the Grid
 			Scene scene = new Scene(totalScreen);
 			scene.setOnKeyPressed(e -> 
 			{
